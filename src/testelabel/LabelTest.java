c74bc1ef9033bcfe3e186ea4b19b710b37011361
ThiagoBagini <60981835+ThiagoBagini@users.noreply.github.com>
2020-03-04 16:02:31 -0300

    Add files via upload

---
+++ src/testelabel/LabelTest.java
@@ -0,0 +1,12 @@
+package testelabel;
+import javax.swing.JFrame;
+public class LabelTest 
+{
+    public static void main(String[] args) 
+    {
+        LabelFrame labelframe = new LabelFrame();
+        labelframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelframe.setSize(260,180);
+        labelframe.setVisible(true);
+    }
+}
